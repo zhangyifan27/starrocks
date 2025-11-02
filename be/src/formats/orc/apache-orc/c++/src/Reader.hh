f5be211131ad63992ec75d58a21d5399b6629c33
xingjieliao <xingjieliao@tencent.com>
2025-11-02 08:03:52 +0000

    --story=128204652 promote SharedBuffer mapping from column streams to RowGroup level (merge request !1372)
    问题背景：
    一个SharedBuffer对应orc stripe里的一个 Column Stream （stream size>=8MB情况下），在扫描时可能驻留整个stripe级别的数据在内存中。
    解决方案：
    1.修改 orc lib层构建 IORange 的逻辑，借助 RowIndex 信息获取 Column Stream 内 RowGroup 的偏移量。
    2.修改 orc lib层发送 read 请求的数据块大小，原本由 min(natural_read_size, column stream size) 决定，为了保证正确性，column stream size需要调整为最小的 row group size
    其他：
    该优化在 https://git.woa.com/doris/starrocks/-/merge_requests/1364 的基础上，可以减少 IO task 10%~20%左右的内存占用

--- be/src/formats/orc/apache-orc/c++/src/Reader.hh
+++ be/src/formats/orc/apache-orc/c++/src/Reader.hh
@@ -148,6 +148,7 @@ private:
 
     // row index of current stripe with column id as the key
     std::unordered_map<uint64_t, proto::RowIndex> rowIndexes;
+    std::unordered_map<uint64_t, uint64_t> minRowGroupSizes;
     std::map<uint32_t, BloomFilterIndex> bloomFilterIndex;
     std::shared_ptr<SearchArgument> sargs;
     std::unique_ptr<SargsApplier> sargsApplier;
@@ -230,6 +231,8 @@ public:
     uint64_t getRowNumber() const override;
 
     void seekToRow(uint64_t rowNumber) override;
+
+    uint64_t getMinRowGroupSize(uint64_t columnId) const;
 
     const FileContents& getFileContents() const;
     bool getThrowOnHive11DecimalOverflow() const;
