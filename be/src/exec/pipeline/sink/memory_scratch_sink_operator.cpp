d4b841714e137839916d95f46174805686da6beb
evaniazhang <evaniazhang@tencent.com>
2025-11-05 12:54:14 +0800

    [Oteam][Enhancement] Report query statistics in all cases

--- be/src/exec/pipeline/sink/memory_scratch_sink_operator.cpp
+++ be/src/exec/pipeline/sink/memory_scratch_sink_operator.cpp
@@ -27,6 +27,11 @@ Status MemoryScratchSinkOperator::prepare(RuntimeState* state) {
 }
 
 void MemoryScratchSinkOperator::close(RuntimeState* state) {
+    // Report audit statistics when the last sinker is closing
+    if (_num_sinkers.fetch_sub(1, std::memory_order_acq_rel) == 1) {
+        state->fragment_ctx()->workgroup()->executors()->driver_executor()->report_audit_statistics(
+                state->query_ctx(), state->fragment_ctx());
+    }
     Operator::close(state);
 }
 
@@ -47,10 +52,6 @@ bool MemoryScratchSinkOperator::is_finished() const {
 
 Status MemoryScratchSinkOperator::set_finishing(RuntimeState* state) {
     _is_finished = true;
-    if (_num_sinkers.fetch_sub(1, std::memory_order_acq_rel) == 1) {
-        state->fragment_ctx()->workgroup()->executors()->driver_executor()->report_audit_statistics(
-                state->query_ctx(), state->fragment_ctx());
-    }
     return Status::OK();
 }
 
