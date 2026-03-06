255bfa6326fae82b12835162bf4884a57feb86f1
lijinbin <lijinbin@tencent.com>
2026-03-06 03:33:50 +0000

    --story=131622334 [BugFix] Disable broadcast join if broadcast_row_limit is set equal or below than 0 (backport #58307) (#58396) (merge request !1480)
    Co-authored-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -3401,6 +3401,10 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
         return broadcastRowCountLimit;
     }
 
+    public void setBroadcastRowCountLimit(long broadcastRowCountLimit) {
+        this.broadcastRowCountLimit = broadcastRowCountLimit;
+    }
+
     public double getBroadcastRightTableScaleFactor() {
         return broadcastRightTableScaleFactor;
     }
